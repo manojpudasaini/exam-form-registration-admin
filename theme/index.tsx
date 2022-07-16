7139b9c9053cdb0a8907177a7fdc7212902fa51c
manojpudasaini <mpudasaini17@gmail.com>
2022-07-16 21:56:57 +0545

    steps added

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,6 +1,9 @@
 import { extendTheme } from "@chakra-ui/react";
-
+import { StepsStyleConfig as Steps } from "chakra-ui-steps";
 const theme = extendTheme({
+  components: {
+    Steps,
+  },
   fonts: {
     heading: `"Lato", sans-serif`,
     body: `"Poppins", sans-serif`,
